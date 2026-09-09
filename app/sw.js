50a074032cf5c62356dcc1a901893b755fb55602
Ming-0119 <1954093179@qq.com>
2026-09-09 21:33:34 +0800

    Show batch-save feedback inside editor after browser regression

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE='freshlife-web-shell-647b5ec61ab2';
+const CACHE='freshlife-web-shell-3472ba2250ac';
 const SHELL=['/app/','/app/index.html','/app/app.css','/app/app.mjs','/app/core.mjs','/app/manifest.webmanifest','/app-icon.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('freshlife-web-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
